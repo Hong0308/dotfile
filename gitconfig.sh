9c1061dde0b7ab79e9d67ee1cbdf3655dca2f232
Hong <genhom123@gmail.com>
2017-02-10 00:30:41 +0800

    add git config core.editor vim

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,3 +3,4 @@ git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+git config --global core.editor vim
