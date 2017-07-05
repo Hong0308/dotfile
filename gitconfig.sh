9a5d50b82ee54822c6ba8c9d9e170cc5788e8cc8
GameHome <genhom123@gmail.com>
2017-07-05 11:23:09 +0800

    update git alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -5,3 +5,6 @@ git config --global color.interactive auto
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 git config --global core.editor vim
 git config --global core.autocrlf input
+git config --global alias.st "status"
+git config --global alias.pl "pull"
+
