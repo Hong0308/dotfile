5fe4a37d1bb0c078e67dc6a809aa287169b17b11
Hong <genhom123@gmail.com>
2017-07-21 12:02:21 +0800

    update gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,5 +7,6 @@ git config --global core.editor vim
 git config --global core.autocrlf input
 git config --global alias.st "status"
 git config --global alias.pl "pull"
+git config --global alias.ch "checkout"
 git config --global alias.dfn "diff master --name-only"
 
