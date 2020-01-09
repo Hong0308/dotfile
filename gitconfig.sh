1b5e6b984bc00ec41df13124da2ff30299a7c921
Hong <hong@verybuy.tw>
2020-01-09 16:06:43 +0800

    add color branch setting

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,9 +2,15 @@ git config --global color.status auto
 git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+
+git config --global color.branch.current "yellow reverse"
+git config --global color.branch.local "yellow"
+git config --global color.branch.remote "green"
+
 git config --global core.editor vim
 git config --global core.autocrlf input
+
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 git config --global alias.st "status"
 git config --global alias.pl "pull"
 git config --global alias.ch "checkout"
